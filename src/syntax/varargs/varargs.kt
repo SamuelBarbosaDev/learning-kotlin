684fd3c05def52d18da64dea49d0f2f2c2f158fb
SamuelBarbosaDev <samueloficial@protonmail.com>
2025-09-19 12:26:31 -0300

    syntax: Estudo sobre `Varargs`

---
+++ src/syntax/varargs/varargs.kt
@@ -0,0 +1,26 @@
+package com.agiotagemltda.collection.learning
+
+fun main(args: Array<String>){
+    val phoneNumber = SavePhoneNumber(
+        "(00) 00000-0000",
+        "(00) 00000-0000",
+        "(00) 00000-0000",
+        "(00) 00000-0000",
+        "(00) 00000-0000",
+        "(00) 00000-0000",
+        "(00) 00000-0000",
+        "(00) 00000-0000"
+    )
+        .showPhoneNumber()
+}
+
+class SavePhoneNumber(
+    vararg phoneNumbers: String
+){
+    private var phoneNumbersList = phoneNumbers
+    fun showPhoneNumber(){
+        for (phoneNumber in this.phoneNumbersList){
+            println("Phone Number: $phoneNumber")
+        }
+    }
+}
